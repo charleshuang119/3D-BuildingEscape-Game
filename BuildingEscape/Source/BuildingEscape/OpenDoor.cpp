d943a7b7e60ffff3722ddb261d3d053b89d8ebc2
Charles Huang <mxdhyj1995@hotmail.com>
2019-10-06 14:02:44 -0400

    Update OpenDoor.cpp

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "OpenDoor.h"
 #include "BuildingEscape.h"
 #include "Engine/World.h"
@@ -9,11 +7,7 @@
 // Sets default values for this component's properties
 UOpenDoor::UOpenDoor()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
