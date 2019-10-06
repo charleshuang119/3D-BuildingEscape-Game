190aa452ad6473dd2ae85eca5e2864f4c8ba7878
Charles Huang <mxdhyj1995@hotmail.com>
2019-10-06 14:03:38 -0400

    Update PositionReport.h

--- BuildingEscape/Source/BuildingEscape/PositionReport.h
+++ BuildingEscape/Source/BuildingEscape/PositionReport.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -23,7 +21,4 @@ protected:
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-		
-	
 };
